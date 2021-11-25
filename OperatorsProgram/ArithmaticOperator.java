cb53e08400769e3ceef8816c8fa8e486f10d3cbf
Madhuri Ghugarkar <94776331+madhurighugarkar@users.noreply.github.com>
2021-11-25 12:55:12 +0530

    Create  ArithmaticOperator.java

---
+++ OperatorsProgram/ArithmaticOperator.java
@@ -0,0 +1,29 @@
+package operatorsProgram;
+
+public class ArithmaticOperator {
+
+	public static void main(String[] args) {
+		
+		 int a=570;
+		 int b=373;
+		 
+		 //addition operator
+		 System.out.println("a+b = " +(a+b));
+		
+		 //subtraction operator
+		 System.out.println("a-b = " +(a-b));
+		 
+		//multipication operator
+		 System.out.println("a*b = " +(a*b));
+		 
+		//division operator
+		 System.out.println("a/b = " +(a/b));
+		 
+		//modul operator
+		 System.out.println("a%b = " +(a%b));
+
+	}
+
+	}
+
+
