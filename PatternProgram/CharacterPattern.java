2d91e296bd15fd4af9cef9d4cf800727a893bcb6
Madhuri Ghugarkar <94776331+madhurighugarkar@users.noreply.github.com>
2021-11-25 12:33:48 +0530

    Create CharacterPattern.java

---
+++ PatternProgram/CharacterPattern.java
@@ -0,0 +1,28 @@
+package patternProgram;
+
+public class CharacterPattern {
+
+	public static void main(String[] args) {
+		
+		char ch='a', n=4;   
+		  
+		for(int i=0; i<n; i++)  
+		{   
+		ch='a';   
+		  
+		for(int j=0; j<=i; j++)  
+		{   
+
+		System.out.print(ch+"");   
+		   
+		ch++;   
+		}   
+		  
+		System.out.println();   
+		}   
+	
+	}
+
+	}
+
+
