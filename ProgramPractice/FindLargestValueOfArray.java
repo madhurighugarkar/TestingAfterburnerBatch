f44dc2176d2bd1cf4b69e3c95f04cca56f210a61
Madhuri Ghugarkar <94776331+madhurighugarkar@users.noreply.github.com>
2021-11-26 10:58:06 +0530

    Add files via upload

---
+++ ProgramPractice/FindLargestValueOfArray.java
@@ -0,0 +1,20 @@
+package programPractice;
+
+public class FindLargestValueOfArray {
+
+	public static void main(String[] args) {
+	
+		int[] arr={28,3,15,9,17,4,23,2,7897};
+		 
+		int val=arr[0];
+ 
+		for(int i=0; i<arr.length; i++){
+			if(arr[i] > val){
+				val=arr[i];
+			}
+		}
+		System.out.println("Largest value in the Given Array is "+ val);
+	
+	}
+
+}
