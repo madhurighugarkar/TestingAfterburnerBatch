d8143f4f61726bfab17d4f334b2132bfb2d4153b
Madhuri Ghugarkar <94776331+madhurighugarkar@users.noreply.github.com>
2021-11-27 13:39:09 +0530

    Create SetExample.java

---
+++ SortedExample/SetExample.java
@@ -0,0 +1,27 @@
+package sortedExample;
+
+import java.util.HashSet;
+import java.util.Set;
+
+
+public class SetExample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		 
+		Set<Integer> set = new HashSet<>();
+		
+		set.add(23);
+		set.add(67);
+		set.add(87);
+		set.add(34);
+		set.add(27);
+		
+		System.out.println(set);
+		
+		
+
+	}
+
+}
