21bec6ddfbda2e8b5ad97ab9e23f69a808ff586f
Madhuri Ghugarkar <94776331+madhurighugarkar@users.noreply.github.com>
2021-11-27 13:18:29 +0530

    Create CharToInteger.java

---
+++ ConvertExample/CharToInteger.java
@@ -0,0 +1,13 @@
+package convertExample;
+
+public class CharToInteger {
+
+	public static void main(String[] args) {
+		
+		char c = 'j';
+        int number = Integer.parseInt(String.valueOf(c));
+        System.out.println(number);
+
+	}
+
+}
