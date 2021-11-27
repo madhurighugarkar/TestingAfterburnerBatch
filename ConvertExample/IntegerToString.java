b8bb22e809ce23e257c284c48ebc9db825d4d3e4
Madhuri Ghugarkar <94776331+madhurighugarkar@users.noreply.github.com>
2021-11-27 13:19:45 +0530

    Add files via upload

---
+++ ConvertExample/IntegerToString.java
@@ -0,0 +1,18 @@
+package convertExample;
+
+public class IntegerToString {
+
+	public static void main(String[] args) {
+		
+		int x = 123;
+        int y = 456;
+        String s1 = Integer.toString(x);
+        String s2 = Integer.toString(y);
+        
+        System.out.println("String s1 = " + s1);
+        System.out.println("String s2 = " + s2); 
+}	
+
+	}
+
+
---
+++ ConvertExample/StringToInteger.java
@@ -0,0 +1,15 @@
+package convertExample;
+
+public class StringToInteger {
+
+	public static void main(String[] args) {
+	
+
+			String str = "100";
+	                // Integer.parseInt()
+	                System.out.println( Integer.parseInt( str ));
+		}	
+
+   }
+
+
