5feba70811e7ca6691cd798d2a27e448595b7344
Madhuri Ghugarkar <94776331+madhurighugarkar@users.noreply.github.com>
2021-11-27 13:30:10 +0530

    Create  DataHidingExample.java

---
+++ oOOPsConcept/DataHidingExample.java
@@ -0,0 +1,18 @@
+package oOOPsConcept;
+
+public class DataHidingExample {
+
+
+		  private static String name;
+		     public static String getNameValue() {
+		    	 return name;
+		     }
+		     
+		     
+		  public static void setNameValue(String s) {
+		    	 name = s;
+		     }
+		
+	}
+
+
