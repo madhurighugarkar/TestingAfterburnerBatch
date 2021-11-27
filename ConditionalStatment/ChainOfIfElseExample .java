2f2d0731ddf959a0f84880a04aec1b287c339884
Madhuri Ghugarkar <94776331+madhurighugarkar@users.noreply.github.com>
2021-11-27 13:23:38 +0530

    Create ChainOfIfElseExample .java

---
+++ ConditionalStatment/ChainOfIfElseExample .java
@@ -0,0 +1,28 @@
+package conditionalStatment;
+
+public class ChainOfIfElseExample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+        char g='m';
+		
+		if(g=='m')
+		{
+			System.out.println("I am a Boy");
+		}
+		else if(g=='f')
+		{
+			System.out.println("I am a Girl");
+		}
+		else
+		{
+			System.out.println("I am not a human being");
+		}
+		
+	}
+
+
+	}
+
+
